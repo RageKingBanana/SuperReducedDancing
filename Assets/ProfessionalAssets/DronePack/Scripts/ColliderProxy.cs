bb95df4dbdb0d65c58747c125df6071c2018240d
NoobMaster <bk201_perez@yahoo.com>
2021-05-24 02:48:10 +0800

    fixed proxy collision script for door 3

--- Assets/ProfessionalAssets/DronePack/Scripts/ColliderProxy.cs
+++ Assets/ProfessionalAssets/DronePack/Scripts/ColliderProxy.cs
@@ -8,12 +8,12 @@ namespace PhysicsPlayground
 
         public GameObject host;
 
-        void OnTriggerEnter(Collider col) { host.SendMessage("OnTriggerEnter", col, SendMessageOptions.DontRequireReceiver); }
-        void OnTriggerStay(Collider col) { host.SendMessage("OnTriggerStay", col, SendMessageOptions.DontRequireReceiver); }
-        void OnTriggerExit(Collider col) { host.SendMessage("OnTriggerExit", col, SendMessageOptions.DontRequireReceiver); }
+        void OnTriggerEnter(Collider col) { Debug.Log("OnTriggerEnter"); }
+        void OnTriggerStay(Collider col) { Debug.Log("OnTriggerStay"); }
+        void OnTriggerExit(Collider col) { Debug.Log("OnTriggerExit");}
 
-        void OnCollisionEnter(Collision col) { host.SendMessage("OnCollisionEnter", col, SendMessageOptions.DontRequireReceiver); }
-        void OnCollisionStay(Collision col) { host.SendMessage("OnCollisionStay", col, SendMessageOptions.DontRequireReceiver); }
-        void OnCollisionExit(Collision col) { host.SendMessage("OnCollisionExit", col, SendMessageOptions.DontRequireReceiver); }
+        void OnCollisionEnter(Collision col) { Debug.Log("OnCollisionEnter"); }
+        void OnCollisionStay(Collision col) { Debug.Log("OnCollisionStay");}
+        void OnCollisionExit(Collision col) { Debug.Log("OnCollisionExit"); }
     }
 }
