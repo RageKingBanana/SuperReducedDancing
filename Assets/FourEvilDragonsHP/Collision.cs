815baf155cb90af1d4b1b59b28f78e032e326ae3
NoobMaster <bk201_perez@yahoo.com>
2021-05-24 01:32:58 +0800

    bat andami neto

---
+++ Assets/FourEvilDragonsHP/Collision.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+            audioSource.Play();
+    }
+}
